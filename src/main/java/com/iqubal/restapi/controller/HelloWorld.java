af6ec678d362ba86095fe73605ba726877e09167
wasimiqubal <wasimiqubal@gmail.com>
2024-09-12 21:21:47 +0530

    Update HelloWorld.java
    Added @RestController

--- src/main/java/com/iqubal/restapi/controller/HelloWorld.java
+++ src/main/java/com/iqubal/restapi/controller/HelloWorld.java
@@ -2,7 +2,7 @@ package com.iqubal.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RestController
 public class HelloWorld {
 	
 	@GetMapping("/hello")
