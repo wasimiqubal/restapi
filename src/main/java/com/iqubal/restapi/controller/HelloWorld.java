4b688b46e093dd53373f5a59be04bb091b2c09c3
wasimiqubal <wasimiqubal@gmail.com>
2024-09-12 18:34:11 +0530

    first commit for restapi project

---
+++ src/main/java/com/iqubal/restapi/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.iqubal.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	@GetMapping("/hello")
+	
+	public String helloWorld() {
+		
+		return " Hello World Jenkins Testing";
+	}
+
+}
